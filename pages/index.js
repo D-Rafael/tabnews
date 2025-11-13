197f5dfe32b0f07e468978a0281a7ee63323b9f5
Diogo Rafael <drs10.trab@gmail.com>
2025-11-13 16:25:53 -0300

    Prettier Configurado

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-   return <h1>Bem vindo a minha página meu amiga!!</h1>
+function Home() {
+  return <h1>Bem vindo a minha página meu amiga!!</h1>;
 }
 
-export default Home
+export default Home;
